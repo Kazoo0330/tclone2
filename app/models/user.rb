656bb4773f1de367deb8752b0d5b556734737723
kazoo <kazuki_saito@diveintocode.jp>
2018-04-20 21:40:15 +0900

    twicloneからアソシエーションでfavorite機能実装1.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
-  has_many :favorites, dependent: :destroy 
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_posts, through: :favorites, source: :post
 end
